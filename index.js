ed8f9f96e2e5f1c837615f2e559a4e0a8b443be6
Ryan Müeller <rmill865@gmail.com>
2017-10-07 02:44:15 +0000

    added base structure

--- index.js
+++ index.js
@@ -1,85 +1,44 @@
-var player = require('play-sound')(opts = {})
+const player = require('play-sound')(opts = {})
+const express = require('express')
+const request = require('request');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/action', (req, res) => {
+  playSceme(req.json);
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
+
+const ip = '192.168.0.136'
+const user = 'api/q5L1lMNztxiKWGQurDZ-hHuyGQ0IAKu42wRoQep6'
+const groupUri = `http://${ip}/${user}/group`;
+
+function playScene(scene) {
   
+}
 
+function lightsOn(group, duration = 0, color = 'default') {
+  const uri = `${groupUri}/${group}/action`
+  const colorJson = getColor(color);
+  const json = { "on": true. "transitiontime": duration }.assign(colorJson);
+  request({ method: 'PUT', uri, json });
+}
 
+function lightsOff(group, duration = 0) {
+  const uri = `${groupUri}/${group}/action`
+  const json = { "on": false. "transitiontime": duration }
+  request({ method: 'PUT', uri, json });
+}
 
-
-// // const OSC = require('osc-js')
-// const express = require('express')
-// const request = require('request');
-//
-//
-// //
-// // const osc = new OSC({ plugin: new OSC.DatagramPlugin() })
-// const app = express()
-// //
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/index.html')
-// })
-//
-//
-// const lights = [false, false, false, false, false];
-// const groupUri = 'http://192.168.0.136/api/q5L1lMNztxiKWGQurDZ-hHuyGQ0IAKu42wRoQep6/groups/0/action';
-// const method = 'PUT';
-// let isOn = true;
-//
-// app.post('/test', (req, res) => {
-//   // const message = new OSC.Message('heartbeat');
-//   // osc.send(message, { port: 7400 })
-//   // setInterval(() => {
-//   //   const lightId = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-//   //   lights[lightId - 1] = !lights[lightId - 1];
-//   //   const lightsUri = `http://192.168.0.136/api/q5L1lMNztxiKWGQurDZ-hHuyGQ0IAKu42wRoQep6/lights/${lightId}/state`;
-//   //   const json = {"on": lights[lightId - 1], "sat":0, "bri":254, "transitiontime": 0};
-//   //   request({method, uri: lightsUri, json});
-//   // }, 200);
-//   if (isOn) {
-//     isOn = false;
-//     turnOffAll();
-//   } else {
-//     isOn = true;
-//     turnOnAll();
-//   }
-// })
-// //
-// app.listen(3000, () => {
-// //   osc.open()
-//   turnOnAll();
-//   console.log('Example app listening on port 3000!')
-// })
-//
-// function turnOffAll() {
-//   const json = {"on": false, "transitiontime": 0};
-//   request({method, uri:groupUri, json});
-// }
-//
-// function turnOnAll() {
-//   const json = {"on": true, "bri": 200, "transitiontime": 0};
-//   request({method, uri:groupUri, json});
-// }
-//
-// // let x = 0;
-// //
-// // setInterval(() => {
-// //   x += (Math.PI / 8);
-// //   const bri = Math.floor((Math.sin(x) + 1) * (255/2));
-// //   const json = {"on": true, "sat":0, bri, "hue":20000, "transitiontime": 0};
-// //   request({method, uri, json});
-// // }, 1000);
-//
-// // let on = true;
-// // setInterval(() => {
-// //   on = !on;
-// //   const json = {on, "sat":255, "bri": 255, "hue":20000};
-// //   request({method, uri, json});
-// // }, 500);
-//
-// // console.log('here');
-// // const json = {"on": true, "sat":0, "bri": 255, "hue":20000, "transitiontime":0};
-// // request({method, uri, json}, () => {
-// //   json.on = false;
-// //   request({method, uri, json}, () => {
-// //     json.on = true;
-// //     request({method, uri, json});
-// //   });
-// // });
+function getColor(color) {
+  switch (color) {
+    case 'white': return { hue: 0, sat: 0, bri: 255 };
+  }
+}
